b131cd091abb61ba3bf536911ef1c4adc5b1722a
Taraneh Mohebi <taraneh.m5257@gmail.com>
2023-01-11 14:13:52 +0330

    fix sendSMS phonefrom env variable

--- pages/api/auth/send-sms.ts
+++ pages/api/auth/send-sms.ts
@@ -6,20 +6,19 @@ export default function sendMessage(req: NextApiRequest, res: NextApiResponse) {
   const { destination, code } = req.body;
   console.log(destination, code);
   if (!destination || !code) {
-    return res
-      .status(422)
-      .json({
-        message: "لطفا اطلاعات مورد نیاز برای انجام عملیات را ارسال نمایید",
-      });
+    return res.status(422).json({
+      message: "لطفا اطلاعات مورد نیاز برای انجام عملیات را ارسال نمایید",
+    });
   }
 
   const accountSid = <string>process.env.twilio_account_sid;
   const token = <string>process.env.twilio_auth_token;
+  const phoneFrom = <string>process.env.app_phone_from;
   const client = twilio(accountSid, token);
   client.messages
     .create({
       body: `your verification code is ${code.toString()}, Good luck!`,
-      from: "+13149882410",
+      from: phoneFrom,
       to: `+98${destination.toString()}`,
     })
     .then((message) =>

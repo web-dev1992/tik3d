0b096fa930b07e39638da3e9577547d03b748889
Taraneh Mohebi <taraneh.m5257@gmail.com>
2023-05-13 21:55:58 +0430

    config db.ts file to show error message whenever atlas mongoose doesn;t response properly

--- helper/db.ts
+++ helper/db.ts
@@ -1,16 +1,29 @@
 import { MongoClient } from "mongodb";
 // const uri = "mongodb://localhost:27017/Tik3D?directConnection=true";
 const connectionString = `mongodb+srv://${process.env.db_username}:${process.env.db_password}@${process.env.db_cluster}.fcm60hd.mongodb.net/${process.env.db_name}?retryWrites=true&w=majority`;
-export async function connectToDatabase() {
+export async function connectToDatabase(fn, res) {
+  const id = setTimeout(
+    () =>
+      res.json({
+        message:
+          "خطایی در سرویس بانک اطلاعاتی mongoose وجود دارد . این خطا مربوط به سایت atlas mongoose است و ارتباطی به سایت tik3dندارد، لطفا مجددا سعی نمایید!",
+      }),
+    7000
+  );
+
   try {
     const client = await MongoClient.connect(
       connectionString
       // "mongodb+srv://taraneh:oX2pnIZtbD7Y4fgU@cluster0.fcm60hd.mongodb.net/Tik3D?retryWrites=true&w=majority"
     );
-    if (client) console.log("connection was established!");
+    if (client) {
+      clearTimeout(id);
+      console.log("connection was established!");
+    }
     return client;
   } catch (err) {
     console.log("an error has happend during connecting to database!");
     console.log(err);
+    res.status(500).json({ message: err.message });
   }
 }
